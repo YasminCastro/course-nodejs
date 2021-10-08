ae2cc9afbd2468b9efc06d5abdee345cf2e2a6f5
Yasmin <yasminsdcastro@gmail.com>
2021-10-08 13:21:32 -0300

    feat: arrow function

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,25 @@
+//maneira de escrever funçãoo arrow
+
+// const square = function (x) {
+//   return x * x;
+// };
+
+// const square = (x) => {
+//   return x * x;
+// };
+
+// const square = (x) => x * x;
+
+const event = {
+  name: "Birthday Party",
+  guestList: ["Andre", "Maycon", "Amanda"],
+  printGuestList() {
+    console.log("Guest list for" + this.name);
+
+    this.guestList.forEach((guest) => {
+      console.log(guest + "irá para " + this.name);
+    });
+  },
+};
+
+event.printGuestList();
