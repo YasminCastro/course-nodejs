079101b93321ac4bb52841ed2fa020fd658ef436
Yasmin <yasminsdcastro@gmail.com>
2021-10-22 11:46:29 -0300

    refactor: tasks

--- 5-task-manager/src/routers/task.js
+++ 5-task-manager/src/routers/task.js
@@ -60,10 +60,11 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id);
+
+    updates.forEach((update) => (task[update] = req.body[update]));
+
+    await task.save();
 
     if (!task) {
       res.status(404).send();
