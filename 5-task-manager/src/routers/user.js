57960843055bc3d02baf014b7d2ca86db21d8853
Yasmin <yasminsdcastro@gmail.com>
2021-10-22 13:12:27 -0300

    feat: users login

--- 5-task-manager/src/routers/user.js
+++ 5-task-manager/src/routers/user.js
@@ -14,6 +14,20 @@ router.post("/users", async (req, res) => {
   }
 });
 
+//user login
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 //fetch multiple users
 router.get("/users", async (req, res) => {
   try {
