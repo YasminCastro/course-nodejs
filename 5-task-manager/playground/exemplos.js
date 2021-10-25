55fc1bf946b711960b4bb7fdb62d3e2dbde73145
Yasmin <yasminsdcastro@gmail.com>
2021-10-25 10:55:25 -0300

    feat: saving methods to remember

---
+++ 5-task-manager/playground/exemplos.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const router = new express.Router();
+
+//route to create a new user
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//user login
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//user logout by token
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//Logout of all sessions
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//fetch multiple users
+router.get("/users", auth, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//allowing user to get their profile
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+//fetch users by id
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  if (_id.length != 24) {
+    res.status(404).send();
+  }
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//route to update user infos by id
+router.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  //Se o usuario tentar alterar alguma informação que não existe ele irá receber um erro
+  const updates = Object.keys(req.body);
+  const allowUpdate = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowUpdate.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Updates." });
+  }
+
+  if (_id.length != 24) {
+    res.status(404).send();
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+
+    await user.save();
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//deleting users by id
+router.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
