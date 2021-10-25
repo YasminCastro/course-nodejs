4524ee1e518558b430f1f2fd68483ad2ece3f099
Yasmin <yasminsdcastro@gmail.com>
2021-10-25 14:25:54 -0300

    feat: schema to delete tasks when user is removed

--- 5-task-manager/src/routers/user.js
+++ 5-task-manager/src/routers/user.js
@@ -110,6 +110,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.deleteOne({ _id: req.user._id });
+
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
