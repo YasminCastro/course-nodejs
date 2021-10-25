b70804bc68dd750325778ef50eaf49f6beb23d53
Yasmin <yasminsdcastro@gmail.com>
2021-10-25 10:50:18 -0300

    refactor: letting users see public profile

--- 5-task-manager/src/models/user.js
+++ 5-task-manager/src/models/user.js
@@ -51,6 +51,17 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+//Public profile
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 //gerando token para usuario
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
