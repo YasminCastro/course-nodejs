27834c22dd4cfe4c5b36e904be721ddab24459ab
Yasmin <yasminsdcastro@gmail.com>
2021-10-26 10:32:10 -0300

    feat: manager system to manage files

--- 5-task-manager/src/routers/user.js
+++ 5-task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 
 const User = require("../models/user");
 const auth = require("../middleware/auth");
@@ -122,7 +123,6 @@ router.delete("/users/me", auth, async (req, res) => {
 
 //path to the image
 const upload = multer({
-  dest: "avatars",
   limits: {
     fileSize: 1000000,
   },
@@ -136,20 +136,57 @@ const upload = multer({
 });
 
 //avatar upload
+//multer is processing the data -> passing through the function -> storing the file data to the avatar field
 router.post(
   "/users/me/avatar",
+  auth,
   upload.single("avatar"),
   async (req, res) => {
-    try {
-      res.send();
-    } catch (error) {
-      res.status(500).send();
-    }
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
+    await req.user.save();
+
+    res.send();
   },
   (error, req, res, next) => {
     //function set up to caught errors
     res.status(400).send({ error: error.message });
   }
 );
+
+//deleting a avatar
+router.delete(
+  "/users/me/avatar",
+  auth,
+  async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+//fetching a avatar
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
+  }
+});
 
 module.exports = router;
